11e66a0fc97d7214b3d161fb45d2eed311a55a42
Matheus Blödorn <blodorn.matheus@gmail.com>
2017-10-04 13:07:30 -0300

    hello anywhere-tools

---
+++ index.js
@@ -0,0 +1,27 @@
+const validateEmail = email => /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+
+const validateCpf = (strCPF) => {
+  let sum
+  let modulus
+  sum = 0
+
+  if (strCPF == '00000000000') { return false }
+  for (let i = 1; i <= 9; i++) { sum += parseInt(strCPF.substring(i - 1, i)) * (11 - i) }
+  modulus = (sum * 10) % 11
+  if ((modulus == 10) || (modulus == 11)) { modulus = 0 }
+  if (modulus != parseInt(strCPF.substring(9, 10))) { return false }
+  sum = 0
+  for (let i = 1; i <= 10; i++) { sum += parseInt(strCPF.substring(i - 1, i)) * (12 - i) }
+  modulus = (sum * 10) % 11
+  if ((modulus == 10) || (modulus == 11)) { modulus = 0 }
+  if (modulus != parseInt(strCPF.substring(10, 11))) { return false }
+  return true
+}
+
+exports.tools =
+  {
+    validations: {
+      validateEmail,
+      validateCpf
+    }
+  }
