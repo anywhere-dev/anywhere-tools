df476427136f199ed64446f48d7e3927b24e8495
Matheus Blödorn <blodorn.matheus@gmail.com>
2017-10-05 15:19:43 -0300

    fix tests for email

--- src/features/email/email.test.js
+++ src/features/email/email.test.js
@@ -18,11 +18,11 @@ describe('Email ', () => {
   describe('Validation()', () => {
 
     it('should validate a real Email', () => {
-      validEmails.map(EMAIL.validEmail => assert(validateEmail(validEmail) == true))
+      validEmails.map(validEmail => assert(EMAIL.validateEmail(validEmail) == true))
     })
 
     it('should invalidate a fake Email', () => {
-      invalidEmails.map(EMAIL.invalidEmail => assert(validateEmail(invalidEmail) == false))
+      invalidEmails.map(invalidEmail => assert(EMAIL.validateEmail(invalidEmail) == false))
     })
 
   })
