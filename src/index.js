713b1451cb23cd5bb87bebb88447872d04ed833a
Matheus Blödorn <blodorn.matheus@gmail.com>
2017-10-05 11:53:27 -0300

    testing webpack build

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import validations from './features/validations/'
 
-export default {
+module.exports = {
   validations
 }
---
+++ test.js
@@ -0,0 +1,3 @@
+const x = require('./dist/anywhere-tools')
+
+console.log(x)
