8bc31eddab867c29ee98e0d5ecd556bec5cf4693
André Lorenz <andreelorenz@gmail.com>
2020-07-23 11:22:55 -0300

    Phone number validation

--- src/features/phone/formatting/format.js
+++ src/features/phone/formatting/format.js
@@ -1,17 +1,8 @@
-const format = phone => {
-    phone = phone.replace(/[^0-9]+/g, "")
-    if (phone.length > 0) {
-        phone = phone.substring(-1, 0) + "(" + phone.substring(0)
-    }
-    if (phone.length > 3) {
-        phone = phone.substring(0, 3) + ")" + phone.substring(3)
-    }
-    if (phone.length == 12) {
-        phone = phone.substring(0, 8) + "-" + phone.substring(8)
-    } else if (phone.length >= 13) {
-        phone = phone.substring(0, 9) + "-" + phone.substring(9, 13)
-    }
-    return phone
-}
+const format = (phone) => {
+  phone = phone.replace(/\D/g, "");
+  phone = phone.replace(/^(\d{2})(\d)/g, "($1) $2");
+  phone = phone.replace(/(\d)(\d{4})$/, "$1-$2");
+  return phone;
+};
 
-export default format
+export default format;
--- src/features/phone/validation/validate.js
+++ src/features/phone/validation/validate.js
@@ -1,3 +1,8 @@
-const validate = phone => false
+const validate = (phone) => {
+  const regex = new RegExp(
+    /\(?\b([0-9]{2}|0((x|[0-9]){2,3}[0-9]{2}))\)?\s*[0-9]{4,5}[- ]*[0-9]{4}\b/
+  );
+  return regex.test(phone);
+};
 
-export default validate
+export default validate;
